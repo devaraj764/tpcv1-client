6b8cbe3471ff26730ddd4517f5956088ccd239e8
Devaraj764 <mdevaraju764@.com>
2022-05-09 17:39:34 +0530

    rigistration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import {
   BrowserRouter as Router,
-  Routes,
-  Route} from 'react-router-dom';
-import Login from './pages/Login.js';
-import Register from './pages/Register.js'
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path='/login' element={< Login />}></Route>
-        <Route exact path='/register' element={< Register />}></Route>
-      </Routes>
+      <Switch>
+        <Route exact path='/login' component={Home}></Route>
+        <Route exact path='/register' component={Home}></Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,208 @@
+import React, { useState, useEffect } from 'react';
+import { Col, Row, Image, InputGroup, FormControl, Button } from 'react-bootstrap';
+import CareerImg from '../assets/career.svg';
+import TPCLogo from '../assets/tpc-logo.svg';
+import { MdPermIdentity, MdEmail, MdPassword, MdBatchPrediction, MdPhone } from 'react-icons/md';
+import { BiRename } from 'react-icons/bi'
+import { FaBirthdayCake, FaBook } from 'react-icons/fa';
+import { SiGoogleclassroom } from 'react-icons/si';
+import { Link, withRouter } from 'react-router-dom';
+
+function Home(props) {
+
+    const [path, setPath] = useState(props.location.pathname);
+
+    useEffect(() => {
+        setPath(props.location.pathname)
+    }, [props.location.pathname]);
+
+    return (
+        <>
+            <Row className="Home" style={{ minHeight: '100vh' }}>
+                <Col className="left-side" lg={6} md={6}>
+                    <div style={{ textAlign: 'center', color: 'white' }}>
+                        <Image src={CareerImg} fluid={true} style={{ maxHeight: '300px', marginBottom: '50px' }} />
+                        <p style={{ fontSize: '20px' }}>“Find out what you like doing best, <br />and get someone to pay you for doing it.”</p>
+                    </div>
+                </Col>
+                <Col className="right-side" sm={12} lg={6} md={6}>
+                    <Image src={TPCLogo} fluid={true} style={{ maxHeight: '300px', marginBottom: '30px' }} />
+                    {path === '/login' ? <Login /> : <Register />}
+
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+function Login(props) {
+    return (
+        <>
+
+            <p style={{ fontSize: '24px', fontWeight: 'bold' }}>Hello Again!</p>
+            <p style={{ color: 'grey', textAlign: 'center', fontSize: '14px' }}>Hey, Enter your credentials to get login to your account..</p>
+            <br />
+            <InputGroup className="mb-3 inputField">
+                <InputGroup.Text id="basic-addon1"><MdPermIdentity /></InputGroup.Text>
+                <FormControl
+                    placeholder="Student Id"
+                    aria-label="Student Id"
+                    aria-describedby="basic-addon1"
+                />
+            </InputGroup>
+            <InputGroup className="mb-3 inputField">
+                <InputGroup.Text id="basic-addon2"><MdPassword /></InputGroup.Text>
+                <FormControl
+                    placeholder="Password"
+                    aria-label="Password"
+                    aria-describedby="basic-addon1"
+                />
+            </InputGroup>
+            <div style={{ maxWidth: '400px', width: '100%' }}>
+                <p style={{ float: 'right', fontSize: '14px' }}>Forgot Password?</p>
+            </div>
+            <br />
+            <Button style={{ maxWidth: '400px', width: '100%', backgroundColor: '#071a84' }} size="md">Login</Button>
+            <br />
+            <p>Don't have account? <Link style={{ color: '#071a84', cursor: 'pointer', fontSize: '16px', fontWeight: '600' }} to="/register">SignUp</Link></p>
+        </>
+    );
+}
+
+function Register(props) {
+    return (
+        <>
+            <p style={{ fontSize: '24px', fontWeight: 'bold' }}>Student Registration Form</p>
+            <br />
+            <Row style={{maxWidth:'756px'}}>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1"><BiRename /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Full Name"
+                            aria-label="Full name"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><MdPermIdentity /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="ID Number"
+                            aria-label="idnumber"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><MdEmail /></InputGroup.Text>
+                        <FormControl
+                            type='email'
+                            placeholder="Email"
+                            aria-label="email"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><FaBirthdayCake /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Date of Birth"
+                            aria-label="ddob"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><MdBatchPrediction /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Batch"
+                            aria-label="Batch"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><FaBook /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Year of study"
+                            aria-label="Yaer of study  "
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><MdPhone /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Contact Number"
+                            aria-label="number"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon3"><SiGoogleclassroom /></InputGroup.Text>
+                        <FormControl
+                            type='text'
+                            placeholder="Enter your section Code"
+                            aria-label="class"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col sm={12}>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            as='textarea'
+                            type='text'
+                            placeholder="Address"
+                            aria-label="Address"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon2"><MdPassword /></InputGroup.Text>
+                        <FormControl
+                            type='password'
+                            placeholder="Password"
+                            aria-label="Password"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col md={6} sm={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon2"><MdPassword /></InputGroup.Text>
+                        <FormControl
+                            type='password'
+                            placeholder="Confirm password"
+                            aria-label="Consirm password"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                </Col>
+            </Row>
+            <br />
+            <Button style={{ maxWidth: '400px', width: '100%', backgroundColor: '#071a84' }} size="md">Proceed</Button>
+            <br />
+            <p>Already had account? <Link style={{ color: '#071a84', cursor: 'pointer', fontSize: '16px', fontWeight: '600' }} to="/login">SignIn</Link></p>
+        </>
+    );
+}
+
+export default withRouter(Home);
