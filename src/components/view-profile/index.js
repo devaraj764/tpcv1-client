d39c45a04e14d15e74243905276fc1cf4ffea4ed
Devaraj764 <mdevaraju764@.com>
2022-06-21 09:45:13 +0530

    View-Profile Done

--- src/components/view-profile/index.js
+++ src/components/view-profile/index.js
@@ -33,12 +33,12 @@ const ViewProfile = (props) => {
                     <Col md={7}>
                         <div className="header">
                             <div className="header-left">
-                                <Image src={profile.imageUrl ? `${props.api}${profile.imageUrl}` : `${props.api}/uploads/default.svg`} alt="profile-image" height="100px" style={{ borderRadius: '10px' }} />
-                            </div>
-                            <div className="header-right">
                                 <p className="user-name">{profile.name}</p>
                                 <p>{profile.email}</p>
                                 <p>+91 {profile.contactNumber}</p>
+                            </div>
+                            <div className="header-right">
+                                <Image src={profile.imageUrl ? `${props.api}${profile.imageUrl}` : `${props.api}/uploads/default.svg`} alt="profile-image" height="100px" style={{ borderRadius: '10px' }} />
                             </div>
                         </div>
                         <hr />
@@ -96,15 +96,146 @@ const ViewProfile = (props) => {
                                     </div>
                                 </Col>
                             </Row>
-                            {/* Skills */}
+                            {/* HardSkills */}
                             <Row className="section">
                                 <Col md={4} sm={12}>
-                                    <p className="heading">Skills</p>
+                                    <p className="heading">Hard Skills</p>
                                 </Col>
                                 <Col md={8} sm={12}>
                                     <div className="description">
-                                        <p className="sub-heading">Hard skills</p>
+                                        {profile.hardSkills.length > 0 ?
+                                            profile.hardSkills.map((skill, id) => {
+                                                return (
+                                                    <div key={id} style={{ marginBottom: '20px' }}>
+                                                        <p className="sub-heading">{skill.title}</p>
+                                                        {skill.data.length > 0 ?
+                                                            skill.data.map((dataItem, id) => (
+                                                                <p key={id}>{dataItem.name}
+                                                                    {dataItem.level ? <> : <i>{dataItem.level}{dataItem.tools ? ` - ${dataItem.tools}` : null}</i></> : null}
+                                                                    {dataItem.description ? <><br /> <span style={{ marginLeft: '10px' }}>{dataItem.description}</span></> : null}
+                                                                </p>
+                                                            )) : null
+                                                        }
+                                                    </div>
+                                                )
 
+                                            })
+                                            : null
+                                        }
+
+                                    </div>
+                                </Col>
+                            </Row>
+                            {/* SoftSkills */}
+                            <Row className="section">
+                                <Col md={4} sm={12}>
+                                    <p className="heading">Soft Skills</p>
+                                </Col>
+                                <Col md={8} sm={12}>
+                                    <div className="description">
+                                        {profile.softSkills.length > 0 ?
+                                            profile.softSkills.map((skill, id) => {
+                                                if (skill.title === 'Language Proficiency') {
+                                                    return (
+                                                        <div key={id} style={{ marginBottom: '20px' }}>
+                                                            <p className="sub-heading">{skill.title}</p>
+                                                            {skill.data.length > 0 ?
+                                                                skill.data.map((dataItem, id) => (
+                                                                    <p key={id}>{dataItem.name}
+                                                                        {dataItem.level ? <> : <i>{dataItem.level}</i></> : null}
+                                                                    </p>
+                                                                )) : null
+                                                            }
+                                                        </div>
+                                                    )
+                                                }
+                                                return (
+                                                    <div key={id} style={{ marginBottom: '20px' }}>
+                                                        <p className="sub-heading">{skill.title} : <i style={{ fontSize: '14px', fontWeight: '500', color: '#6b818b' }}>{skill.level}</i></p>
+                                                    </div>
+                                                )
+
+                                            })
+                                            : null
+                                        }
+
+                                    </div>
+                                </Col>
+                            </Row>
+                            {/* projects */}
+                            <Row className="section">
+                                <Col md={4} sm={12}>
+                                    <p className="heading">Projects</p>
+                                </Col>
+                                <Col md={8} sm={12}>
+                                    <div className="description">
+                                        {profile.projects.length > 0 ? profile.projects.map((project) => (
+                                            <div style={{ marginBottom: '20px' }} key={project.title}>
+                                                <p className="sub-heading">{project.title}</p>
+                                                <p>{project.description}</p>
+                                                <p>Techinologies used : {project.technologies}</p>
+                                                {project.link ? <a href={project.link}>Know more..</a> : null}
+                                            </div>
+                                        )) : <p>Not Updated</p>
+                                        }
+                                    </div>
+                                </Col>
+                            </Row>
+                            {/* Intenships */}
+                            <Row className="section">
+                                <Col md={4} sm={12}>
+                                    <p className="heading">Internships</p>
+                                </Col>
+                                <Col md={8} sm={12}>
+                                    <div className="description">
+                                        {profile.internships.length > 0 ? profile.internships.map((internship) => (
+                                            <div style={{ marginBottom: '20px' }} key={internship.organization}>
+                                                <p className="sub-heading">Role : {internship.role}</p>
+                                                <p>Company : {internship.organization}</p>
+                                                <p>Duration : {internship.duration} months</p>
+                                                <p>Status : {internship.status}</p>
+                                                <p>{internship.status === 'working' ? `Joined on ${internship.startDate}` : `${internship.startDate} - ${internship.endDate}`}</p>
+                                                {profile.certificate ? <a href={profile.certificate}>View certificate</a> : null}
+                                            </div>
+                                        )) : <p>Not Updated</p>
+                                        }
+                                    </div>
+                                </Col>
+                            </Row>
+                            {/* Certifications */}
+                            <Row className="section">
+                                <Col md={4} sm={12}>
+                                    <p className="heading">Certifications</p>
+                                </Col>
+                                <Col md={8} sm={12}>
+                                    <div className="description">
+                                        {profile.certifications.length > 0 ? profile.certifications.map((item) => (
+                                            <div style={{ marginBottom: '20px' }} key={item.title}>
+                                                <p className="sub-heading">{item.title}</p>
+                                                <p>Certiified by {item.organization}</p>
+                                                <p>Status : {item.status}</p>
+                                                <p>{item.status === 'working' ? `Joined on ${item.startDate}` : `${item.startDate} - ${item.endDate}`}</p>
+                                                {item.link ? <a href={item.link}>View certificate</a> : null}
+                                            </div>
+                                        )) : <p>Not Updated</p>
+                                        }
+                                    </div>
+                                </Col>
+                            </Row>
+                            {/* Achievements */}
+                            <Row className="section">
+                                <Col md={4} sm={12}>
+                                    <p className="heading">Achievements</p>
+                                </Col>
+                                <Col md={8} sm={12}>
+                                    <div className="description">
+                                        {profile.achievements.length > 0 ? profile.achievements.map((project) => (
+                                            <div style={{ marginBottom: '20px' }} key={project.title}>
+                                                <p className="sub-heading">{project.title}</p>
+                                                <p>{project.description}</p>
+                                            </div>
+                                        )) : <p>Not Updated</p>
+                                        }
                                     </div>
                                 </Col>
                             </Row>
