024a886111522b0e7a6cb6cd9927c98c2d9fd735
Devaraj764 <mdevaraju764@.com>
2022-06-24 16:36:55 +0530

    Changes

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const uri = 'https://matched-perl-cons-operates.trycloudflare.com/'
+
 // myAxios.js
 const instance = axios.create({
-    baseURL: 'https://tpc-api.loca.lt',
+    baseURL: uri,
     headers: {
         common: {
             "Bypass-Tunnel-Reminder": 'true'
